08e55da520dca55a561d515d2b4891da2908f4f7
Disha Phalle <163996994+dishaphalle27@users.noreply.github.com>
2024-05-28 18:54:26 +0530

    Update Titanic Survival Predictor app.py

--- Titanic Survival Predictor app.py
+++ Titanic Survival Predictor app.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import streamlit as st
 import pandas as pd
+
+# Load the predictions DataFrame
 predictions_df = pd.read_csv(r'C:\Users\disha\Downloads\titanic_predictions.csv')
+
 def main():
+    # Set the title of the app
     st.title('Titanic Survival Predictor')
+
+    # Display the predictions DataFrame
+    st.subheader('Predictions DataFrame')
+    st.write(predictions_df)
+
+    # Display basic statistics about the predictions
+    st.subheader('Predictions Statistics')
+    st.write("Number of predictions:", len(predictions_df))
+    st.write("Number of survivors:", predictions_df['Survived'].sum())
+    st.write("Number of non-survivors:", len(predictions_df) - predictions_df['Survived'].sum())
+
 if __name__ == '__main__':
     main()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
