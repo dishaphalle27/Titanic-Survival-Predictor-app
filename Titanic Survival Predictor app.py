33f916e2b8320daab4b2a8eb84caa18b422b838b
Disha Phalle <163996994+dishaphalle27@users.noreply.github.com>
2024-05-28 18:41:17 +0530

    Add files via upload

---
+++ Titanic Survival Predictor app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import streamlit as st
+import pandas as pd
+predictions_df = pd.read_csv(r'C:\Users\disha\Downloads\titanic_predictions.csv')
+def main():
+    st.title('Titanic Survival Predictor')
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
